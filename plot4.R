9843747cfe71d9a74bbfbd9415c0d22bde5cb808
evagradovich <33654687+evagradovich@users.noreply.github.com>
2018-06-20 18:34:06 +0100

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -19,7 +19,7 @@ plot(datetime, plot_data1, type="l", xlab="", ylab="Global Active Power", cex=0.
 
 plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
 
-plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+plot(datetime, subMetering1, type="l", ylab="Energy sub metering", xlab="")
 lines(datetime, subMetering2, type="l", col="red")
 lines(datetime, subMetering3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
